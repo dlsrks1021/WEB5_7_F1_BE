687c75e8c2054e6339142e09bd0e337c1b6f0984
dlsrks1021 <dlsrks1021@gmail.com>
2026-01-26 10:50:11 +0000

    feat: 빌드 환경 구성

---
+++ consumers/stat-consumer/src/main/java/io/f1/stat_consumer/StatConsumerApplication.java
@@ -0,0 +1,13 @@
+package io.f1.stat_consumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StatConsumerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StatConsumerApplication.class, args);
+	}
+
+}
---
+++ consumers/stat-consumer/src/test/java/io/f1/stat_consumer/StatConsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package io.f1.stat_consumer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StatConsumerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
